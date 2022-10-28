3127107af8abbf0eb7247a6092d77a1aad85c848
frazeinky <rifkyzena26@gmail.com>
2022-10-28 21:45:46 +0700

    confetti module

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { startConfetti, stopConfetti, removeConfetti } from './confetti.js';
+
 const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
@@ -35,6 +37,8 @@ function resetSelected () {
   allGameIcons.forEach ((icon) => {
     icon.classList.remove('selected');
   });
+  stopConfetti();
+  removeConfetti();
 }
 
 // Reset score & playerChoice/computerChoice
@@ -101,6 +105,7 @@ function updateScore(playerChoice){
   } else {
     const choice = choices[playerChoice];
     if (choice.defeats.indexOf(computerChoice) > -1){
+      startConfetti();
       resultText.textContent = "You Won! :D";
       playerScoreNumber++;
       playerScoreEl.textContent = playerScoreNumber;
@@ -149,3 +154,7 @@ function select(playerChoice) {
       break;
   }
 }
+window.select = select;
+
+// On startup, set initial values
+resetAll();
